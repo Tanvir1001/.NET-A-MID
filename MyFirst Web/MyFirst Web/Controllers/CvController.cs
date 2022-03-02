588659c1bf17023006d71f8d1d98e8e74a1d9451
Tanvir Sarrick <tanvirsarrick@gmail.com>
2022-03-02 19:30:06 +0600

    first commit

---
+++ MyFirst Web/MyFirst Web/Controllers/CvController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirst_Web.Controllers
+{
+    public class CvController : Controller
+    {
+        // GET: Cv
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
